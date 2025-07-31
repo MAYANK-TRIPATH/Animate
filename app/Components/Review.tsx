5b8221fe4c1d52cdef12a58e1160625ab63072dc
MAYANK-TRIPATH <mr.mayanktripathi2001@gmail.com>
2025-07-31 19:28:12 +0530

    rr

--- app/Components/Review.tsx
+++ app/Components/Review.tsx
@@ -1,12 +1,12 @@
 export default function Review() {
     return (
-        <div className="flex justify-center items-center p-8 mb-24 mt-24">
-            <div className="bg-gradient-to-b from-blue-300 to-[rgb(24,166,213)] rounded-3xl p-8 max-w-2xl h-100">
+        <div className="flex justify-center items-center p-4 sm:p-8 mb-12 sm:mb-24 mt-12 sm:mt-24">
+            <div className="bg-gradient-to-b from-blue-300 to-[rgb(24,166,213)] rounded-3xl p-6 sm:p-8 max-w-2xl min-h-[200px] sm:h-auto">
                 <div className="text-white">
-                    <p className="text-xl leading-relaxed mb-6 text-left font-bold">
+                    <p className="text-lg sm:text-xl leading-relaxed mb-4 sm:mb-6 text-left font-bold">
                         I chose Thoughtly because it was easy, seamless, and most importantly, extremely affordable. It took a few weeks to get set up due to my other commitments, but once we launched, it was seamless and I received white glove service from day one.
                     </p>
-                    <p className="text-xs text-left lg:mt-40">
+                    <p className="text-xs text-left mt-8 sm:mt-12 lg:mt-40">
                         PRESIDENT AT LEADING HOME SERVICES COMPANY
                     </p>
                 </div>
